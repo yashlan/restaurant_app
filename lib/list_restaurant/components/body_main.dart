ae5f087862180b57d91c2b717f6f5f5a9f5dbd2a
yashlan <yashlan007@gmail.com>
2023-01-31 17:55:55 +0700

    Create body_main.dart

---
+++ lib/list_restaurant/components/body_main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:restaurant_app/list_restaurant/components/head.dart';
+import 'package:restaurant_app/list_restaurant/components/restaurant_list.dart';
+
+class BodyMain extends StatelessWidget {
+  final int gridCount;
+  const BodyMain({
+    super.key,
+    this.gridCount = 0,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: <Widget>[
+          const Header(),
+          gridCount == 0
+              ? const RestaurantListView()
+              : RestaurantGridView(gridCount: gridCount),
+        ],
+      ),
+    );
+  }
+}
