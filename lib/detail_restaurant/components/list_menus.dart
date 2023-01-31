5b47703f9df7ba71587d0c475a7206f7bf9bb6a3
yashlan <yashlan007@gmail.com>
2023-01-31 23:15:36 +0700

    Update list_menus.dart

--- lib/detail_restaurant/components/list_menus.dart
+++ lib/detail_restaurant/components/list_menus.dart
@@ -3,89 +3,85 @@ import 'package:flutter/material.dart';
 import 'package:restaurant_app/model/local_restaurant.dart';
 import 'package:restaurant_app/utils/dialog_utils.dart';
 
+enum MenuType { foods, drinks }
+
 class ListMenus extends StatelessWidget {
   final Restaurant restaurant;
+  final MenuType menuType;
   const ListMenus({
     Key? key,
     required this.restaurant,
+    required this.menuType,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     final ScrollController controller = ScrollController();
-    return Align(
-      alignment: Alignment.centerLeft,
-      child: Column(
-        mainAxisSize: MainAxisSize.min,
-        children: <Widget>[
-          SizedBox(
-            height: 100,
-            child: ScrollConfiguration(
-              behavior: MyCustomScrollBehavior(),
-              child: ListView.builder(
-                controller: controller,
-                physics: const ClampingScrollPhysics(),
-                shrinkWrap: true,
-                scrollDirection: Axis.horizontal,
-                itemCount: restaurant.menus.foods.length,
-                itemBuilder: (context, index) {
-                  final String foodName = restaurant.menus.foods[index].name;
-                  return InkWell(
-                    onTap: () {
-                      showAlertDialog(
-                          context, 'anda memilih makanan: $foodName');
-                    },
-                    child: Card(
+    var size = MediaQuery.of(context).size;
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: <Widget>[
+        SizedBox(
+          width: size.width,
+          height: 100,
+          child: ScrollConfiguration(
+            behavior: MyCustomScrollBehavior(),
+            child: ListView.builder(
+              controller: controller,
+              physics: const ClampingScrollPhysics(),
+              shrinkWrap: true,
+              scrollDirection: Axis.horizontal,
+              itemCount: menuType == MenuType.foods
+                  ? restaurant.menus.foods.length
+                  : restaurant.menus.drinks.length,
+              itemBuilder: (context, index) {
+                final String menuName = menuType == MenuType.foods
+                    ? restaurant.menus.foods[index].name
+                    : restaurant.menus.drinks[index].name;
+                return InkWell(
+                  onTap: () {
+                    showAlertDialog(
+                        context, 'anda memilih ${menuType.name}: $menuName');
+                  },
+                  child: Padding(
+                    padding: const EdgeInsets.only(right: 15.0),
+                    child: Container(
+                      width: 160,
+                      height: 100,
+                      alignment: Alignment.bottomCenter,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(12),
+                        image: DecorationImage(
+                          image: NetworkImage(restaurant.pictureId),
+                          fit: BoxFit.cover,
+                        ),
+                      ),
                       child: Container(
-                        color: Colors.amber,
-                        child: Center(
-                          child: Text(
-                            foodName,
-                            style: TextStyle(color: Colors.white),
+                        width: double.infinity,
+                        decoration: BoxDecoration(
+                          color: Colors.black.withOpacity(.5),
+                          borderRadius: const BorderRadius.only(
+                            bottomLeft: Radius.circular(12),
+                            bottomRight: Radius.circular(12),
+                          ),
+                        ),
+                        padding: const EdgeInsets.all(8),
+                        child: Text(
+                          menuName,
+                          style: const TextStyle(
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white,
                           ),
                         ),
                       ),
                     ),
-                  );
-                },
-              ),
+                  ),
+                );
+              },
             ),
           ),
-          SizedBox(
-            height: 100,
-            child: ScrollConfiguration(
-              behavior: MyCustomScrollBehavior(),
-              child: ListView.builder(
-                controller: controller,
-                physics: const ClampingScrollPhysics(),
-                shrinkWrap: true,
-                scrollDirection: Axis.horizontal,
-                itemCount: restaurant.menus.drinks.length,
-                itemBuilder: (context, index) {
-                  final String drinkName = restaurant.menus.drinks[index].name;
-                  return InkWell(
-                    onTap: () {
-                      showAlertDialog(
-                          context, 'anda memilih minuman: $drinkName');
-                    },
-                    child: Card(
-                      child: Container(
-                        color: Colors.redAccent,
-                        child: Center(
-                          child: Text(
-                            drinkName,
-                            style: TextStyle(color: Colors.white),
-                          ),
-                        ),
-                      ),
-                    ),
-                  );
-                },
-              ),
-            ),
-          ),
-        ],
-      ),
+        ),
+      ],
     );
   }
 }
